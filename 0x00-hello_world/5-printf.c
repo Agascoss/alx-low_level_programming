0bf4ca302b720c8b7baadb939de0549e391bb9cf
Agascoss <Danielagadzi302@gmail.com>
2023-03-18 15:23:31 -0700

    the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function prints a message to the console using the puts function.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
