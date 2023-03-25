adf07df04d13908bd3b178e75941ed5a063e4c40
Agascoss <Danielagadzi302@gmail.com>
2023-03-24 18:45:29 -0700

    print most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ *
+ * Return: 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
