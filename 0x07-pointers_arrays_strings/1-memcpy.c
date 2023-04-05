9651e471ba06594758514bf41531df8df0c98bc8
Agascoss <Danielagadzi302@gmail.com>
2023-04-04 18:15:48 -0700

    for task 1 memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _memcpy - Copies a specified number of bytes from one memory location to another.
+ *
+ * @dest: Pointer to the destination  memory location.
+ *
+ * @src: Pointer to the source memory location.
+ *
+ * @n: Number of bytes to be copied.
+ *
+ * Return: Pointer to the destination memory.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
